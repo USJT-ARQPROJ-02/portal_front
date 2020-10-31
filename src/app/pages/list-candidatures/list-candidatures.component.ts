ba842e1ea50b82e40443eee3e264eb9fd48e6f2c
vitvbs <victor.silva@gold360.com>
2020-10-31 09:39:44 -0300

    map integration

--- src/app/pages/list-candidatures/list-candidatures.component.ts
+++ src/app/pages/list-candidatures/list-candidatures.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NecessityService } from 'src/app/services/necessity/necessity.service';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-list-candidatures',
   templateUrl: './list-candidatures.component.html',
@@ -37,10 +39,15 @@ export class ListCandidaturesComponent implements OnInit {
     })
 
     this.necessityService.getFinished().subscribe( (result : any)=> {
-      console.log(result.body)
       result.body.forEach(necessity => {
         this.finishedNecessities.push(necessity)
       });
+
+      this.necessities.forEach(necessity => {
+        necessity.data_inicio = moment(necessity.data_inicio).format('DD/MM/YY')
+        necessity.data_fim = moment(necessity.data_fim).format('DD/MM/YY')
+
+    });
     })
   }
 
